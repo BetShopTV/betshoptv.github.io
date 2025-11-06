6c615cab3beac7b32486c1e604beaf2e6f266764
BetShopTV <betshoptv@gmail.com>
2025-11-06 04:54:52 -0300

    Update alpineApp.js

--- js/alpineApp.js
+++ js/alpineApp.js
@@ -1,109 +1,39 @@
 function appData() {
   return {
-    // Página atual (baseada no hash da URL)
     pagina: 'galeria',
-
-    // Dados carregados dos JSONs
     pecas: [],
     rifas: [],
-
-    // Estado de seleção
-    selecionadas: [],         // IDs de peças
-    favoritas: [],            // IDs de favoritas
-    rifasSelecionadas: [],    // IDs de rifas
-
-    // Dados do usuário
+    selecionadas: [],
+    favoritas: [],
+    rifasSelecionadas: [],
     formulario: {
       nome: '',
       email: '',
       telefone: ''
     },
 
-    // Total da compra
-    totalGeral: 0,
+    get totalGeral() {
+      const totalPecas = this.selecionadas.reduce((sum, peca) => sum + (peca.preco || 0), 0);
+      const totalRifas = this.rifasSelecionadas.length * 5;
+      return totalPecas + totalRifas;
+    },
 
-    // ========== INICIALIZAÇÃO ==========
-    async init() {
+    init() {
+      this.carregarJSONs();
+    },
+
+    async carregarJSONs() {
       try {
-        const [pecasRes, rifasRes] = await Promise.all([
-          fetch('https://betshoptv.com/dados/pecas.json'),
-          fetch('https://betshoptv.com/dados/rifas.json')
-        ]);
+        const pecasData = await fetch('https://betshoptv.com/json/pecas.json').then(res => res.json());
+        const rifasData = await fetch('https://betshoptv.com/json/rifas.json').then(res => res.json());
 
-        this.pecas = await pecasRes.json();
-        this.rifas = await rifasRes.json();
+        this.pecas = pecasData;
+        this.rifas = rifasData;
 
-        this.atualizarTotal();
-        this.syncHashComPagina();
-
-        // Escutar mudanças no hash
-        window.addEventListener('hashchange', () => this.syncHashComPagina());
-
+        console.log('Dados carregados com sucesso ✅');
       } catch (err) {
-        console.error('Erro ao carregar os dados:', err);
+        console.error('Erro ao carregar os JSONs ❌', err);
       }
-    },
-
-    // ========== NAVEGAÇÃO ==========
-    mudarPagina(p) {
-      window.location.hash = p;
-    },
-
-    syncHashComPagina() {
-      const hash = window.location.hash.replace('#', '').trim();
-      const [paginaBase] = hash.split('?');
-      this.pagina = paginaBase || 'galeria';
-    },
-
-    // ========== CONTROLE DE SELEÇÃO ==========
-    toggleSelecionada(id) {
-      const i = this.selecionadas.indexOf(id);
-      if (i === -1) this.selecionadas.push(id);
-      else this.selecionadas.splice(i, 1);
-      this.atualizarTotal();
-    },
-
-    toggleFavorita(id) {
-      const i = this.favoritas.indexOf(id);
-      if (i === -1) this.favoritas.push(id);
-      else this.favoritas.splice(i, 1);
-    },
-
-    toggleRifa(id) {
-      const i = this.rifasSelecionadas.indexOf(id);
-      if (i === -1) this.rifasSelecionadas.push(id);
-      else this.rifasSelecionadas.splice(i, 1);
-      this.atualizarTotal();
-    },
-
-    // ========== TOTAL ==========
-    atualizarTotal() {
-      let total = 0;
-
-      // Soma preços das peças selecionadas
-      this.selecionadas.forEach(id => {
-        const peca = this.pecas.find(p => p.id_peca === id);
-        if (peca && peca.preco) {
-          total += parseFloat(peca.preco);
-        }
-      });
-
-      // Soma valor das rifas (regra par/ímpar)
-      const nR = this.rifasSelecionadas.length;
-      if (nR > 0) {
-        total += (nR % 2 === 0)
-          ? nR * 25
-          : (nR - 1) * 25 + 30;
-      }
-
-      this.totalGeral = total;
-    },
-
-    // ========== FORMATADOR ==========
-    formatarPreco(v) {
-      const n = Number(v || 0);
-      const inteiro = Math.round(n);
-      return 'R$ ' + inteiro.toLocaleString('pt-BR');
     }
   };
 }
